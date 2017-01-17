3cc1a70158a7f85f8608dfc768cad8c121ee22ca
Harald Niemeczek <harald@niemeczek.at>
2017-01-17 20:54:52 +0100

    minor fix

---
+++ template/fotografie/fotografie-footer.php
@@ -0,0 +1,8 @@
+							<ul>
+								<li><a href="/photography">Fotografie</a></li>
+								<li><a href="/photography/timelapse.php">Timelapse</a></li>
+								<li><a href="/photography/starlapse.php">Starlapse</a></li>
+								<li><a href="/photography/malenmitlicht.php">Malen mit Licht</a></li>
+								<li><a href="/photography/naturfotografie/">Natur&shyfotografie</a></li>
+								<li><a href="/photography/software.php">Soft- und Hardware</a></li>
+							</ul>
