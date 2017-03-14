93fba8ce517a35f7ec40dd6aa4db606cd113cdcb
Harald Niemeczek <harald@niemeczek.at>
2017-03-14 15:42:17 +0100

    new images @ naturfotografie/insects1

---
+++ photography/naturfotografie/insekten1.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php 
+
+			include "../../template/head/head.php";
+			include "../../template/head/lightbox.php";
+		?>
+	</head>
+	<body>
+		<?php 
+			include '../../template/header/header.php';
+		?>
+
+		<section class="tabellen_zelle maximale_hoehe">
+		
+				<div id="headerbildhintergrund">
+				<?php 
+					include '../../template/headerbild/headerbild.php';
+				?>	
+								
+				</div>
+				<div id="content_area">
+					<div id="linke_spalte"> <!-- linke Spalte -->
+						<div class="databox newsbox">
+							<?php 
+								include '../../template/fotografie/fotografie.php';
+							?>	
+							
+
+						</div>
+						
+					</div>	
+					<div id="haupt_spalte"> <!-- haupt Spalte -->	
+						<div class="databox contentbox">
+							<h1>Naturfotografie</h1>
+							<p>
+								
+								<br /><br />
+								<br /><br />
+							</p>
+								<div class="imagedatabox"> <!-- Grashalm mit Wassertropfen -->
+									<div class="imagedatabox-image">
+										<a href="/images/fotografie/naturfotografie/insekten/SchmetterlingaufLavendel1.jpg" data-lightbox="bild-2" data-title="Schmetterling sitzt auf Lavendelblüte, Querformat">
+   										<img class="img-full" src="/images/fotografie/naturfotografie/insekten/SchmetterlingaufLavendel1-small.jpg" alt="Gras mit Tautropfen, Desktophintergrund">
+ 										</a>
+									</div>
+									<div class="imagedatabox-description">	
+										<p>
+											<b>Schmetterling auf Lavendelblüte</b><br />
+											<b>Datum:</b>25. Juni 2016<br />
+											<b>Ort:</b> Südl. Kroatien<br />
+											<b>Kamera:</b> NIKON D3200<br />
+											<b>Objektiv:</b> Tamron AF70-300mm F/4-5.6 Di LD MACRO 1:2<br />
+											<b>Blende:</b> F/7,1<br />
+											<b>Belichtungszeit:</b> 1/320<br />	
+											<b>Brennweite:</b> 240 mm<br />
+											<b>ISO:</b> 100<br />
+											
+										</p>
+									</div>
+								</div>
+								<div class="imagedatabox"> <!-- Wiese im Morgenlicht -->
+									<div class="imagedatabox-image">
+										<a href="/images/fotografie/naturfotografie/insekten/SchmetterlingaufLavendel2.jpg" data-lightbox="bild-2" data-title="Gras mit Tautropfen, Desktophintergrund">
+   										<img class="img-full" src="/images/fotografie/naturfotografie/insekten/SchmetterlingaufLavendel2-small.jpg" alt="Gras mit Tautropfen, Desktophintergrund">
+ 										</a>
+									</div>
+									<div class="imagedatabox-description">	
+										<p>
+											<b>Schmetterling auf Lavendelblüte</b><br />
+											<b>Datum:</b>25. Juni 2016<br />
+											<b>Ort:</b> Südl. Kroatien<br />
+											<b>Kamera:</b> NIKON D3200<br />
+											<b>Objektiv:</b> Tamron AF70-300mm F/4-5.6 Di LD MACRO 1:2<br />
+											<b>Blende:</b> F/7,1<br />
+											<b>Belichtungszeit:</b> 1/320<br />	
+											<b>Brennweite:</b> 240 mm<br />
+											<b>ISO:</b> 100<br />
+											
+										</p>
+									</div>
+								</div>
+								<div class="imagedatabox"> <!-- Allium -->
+									<div class="imagedatabox-image">
+										<a href="/images/fotografie/naturfotografie/insekten/HummelaufLavendel1.jpg" data-lightbox="bild-2" data-title="Gras mit Tautropfen, Desktophintergrund">
+   										<img class="img-full" src="/images/fotografie/naturfotografie/insekten/HummelaufLavendel1-small.jpg" alt="Gras mit Tautropfen, Desktophintergrund">
+ 										</a>
+									</div>
+									<div class="imagedatabox-description">	
+										<p>
+											<b>Schmetterling auf Lavendelblüte</b><br />
+											<b>Datum:</b>25. Juni 2016<br />
+											<b>Ort:</b> Südl. Kroatien<br />
+											<b>Kamera:</b> NIKON D3200<br />
+											<b>Objektiv:</b> Tamron AF70-300mm F/4-5.6 Di LD MACRO 1:2<br />
+											<b>Blende:</b> F/7,1<br />
+											<b>Belichtungszeit:</b> 1/320<br />	
+											<b>Brennweite:</b> 240 mm<br />
+											<b>ISO:</b> 100<br />
+											
+										</p>
+									</div>
+								</div>
+								<div class="imagedatabox"> <!-- Eingezäunte Wiese -->
+									<div class="imagedatabox-image">
+										<a href="/images/fotografie/naturfotografie/insekten/HummelaufLavendel2.jpg" data-lightbox="bild-2" data-title="Gras mit Tautropfen, Desktophintergrund">
+   										<img class="img-full" src="/images/fotografie/naturfotografie/insekten/HummelaufLavendel2-small.jpg" alt="Gras mit Tautropfen, Desktophintergrund">
+ 										</a>
+									</div>
+									<div class="imagedatabox-description">	
+										<p>
+											<b>Schmetterling auf Lavendelblüte</b><br />
+											<b>Datum:</b>25. Juni 2016<br />
+											<b>Ort:</b> Südl. Kroatien<br />
+											<b>Kamera:</b> NIKON D3200<br />
+											<b>Objektiv:</b> Tamron AF70-300mm F/4-5.6 Di LD MACRO 1:2<br />
+											<b>Blende:</b> F/7,1<br />
+											<b>Belichtungszeit:</b> 1/320<br />	
+											<b>Brennweite:</b> 240 mm<br />
+											<b>ISO:</b> 100<br />
+											
+										</p>
+									</div>
+								</div>
+								
+								<br /><br />
+							
+						</div>
+					</div>					
+	
+				</div>
+	
+		</section>
+		<?php 
+			include '../../template/footer/footer.php';
+		?>	
+		<script src="/js/lightbox-plus-jquery.min.js"></script>
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+<p class="test">
+			Lorem ipsum dolor sit amet, consecteteur adipiscing elit fermentum cras taciti eni, libero, 
+			ante pede cum. Nulla phasellus hac. Vel curabitur pharetra, semper parturient ipsum. Integer
+			nunc taciti morbi eu turpis mauris, proin euismod cursus. Vivamus ac, cursus leo ut. Nulla 
+			quisque. Massa varius in dui elementum facilisi. Eros sed velit nostra. Ac, ridiculus metus, 
+			eu montes integer at, pede mi. Porttitor curae ipsum taciti suspendisse. Venenatis libero litora 
+			tristique nam, etiam velit, blandit ad, proin ad eu tellus blandit. Orci elit habitasse varius, mi 
+			praesent integer. Convallis. Proin enim ac magna suspendisse habitant, tristique diam orci non cum.
+		</p>
+	-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+<p class="test">
+			Lorem ipsum dolor sit amet, consecteteur adipiscing elit fermentum cras taciti eni, libero, 
+			ante pede cum. Nulla phasellus hac. Vel curabitur pharetra, semper parturient ipsum. Integer
+			nunc taciti morbi eu turpis mauris, proin euismod cursus. Vivamus ac, cursus leo ut. Nulla 
+			quisque. Massa varius in dui elementum facilisi. Eros sed velit nostra. Ac, ridiculus metus, 
+			eu montes integer at, pede mi. Porttitor curae ipsum taciti suspendisse. Venenatis libero litora 
+			tristique nam, etiam velit, blandit ad, proin ad eu tellus blandit. Orci elit habitasse varius, mi 
+			praesent integer. Convallis. Proin enim ac magna suspendisse habitant, tristique diam orci non cum.
+		</p>
+	-->
