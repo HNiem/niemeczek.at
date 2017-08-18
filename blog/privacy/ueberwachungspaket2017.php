1c80f9cadf8d8ce7d3a30abecead0ca5b1520892
Harald Niemeczek <postmaster@niemeczek.at>
2017-08-18 17:50:52 +0200

    added ueberwachungspaket2017.php

---
+++ blog/privacy/ueberwachungspaket2017.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php 
+
+			include "../../template/head/head.php";
+			include "../../template/head/lightbox.php";
+		?>
+	</head>
+	<body>
+		<?php 
+			include '../../template/header/header.php';
+		?>
+
+		<section class="tabellen_zelle maximale_hoehe">
+		
+				<div id="headerbildhintergrund">
+				<?php 
+					include '../../template/headerbild/headerbild.php';
+				?>	
+								
+				</div>
+				<div id="content_area">
+					<div id="linke_spalte"> <!-- linke Spalte -->
+						<div class="databox newsbox">
+							<?php 
+								include '../../template/blog/privacy/privacy.php';
+							?>	
+							
+
+						</div>
+						
+					</div>	
+					<div id="haupt_spalte"> <!-- haupt Spalte -->	
+						<div class="databox contentbox">
+							<h1>Überwachungspaket 2017</h1>
+							<p><br /><br />
+							Bis 21.08 können noch Stellungnahmen zum Überwachungspaket abgegeben werden.
+							<br /><br />
+							<a href="https://epicenter.works/" hreflang="de">Epicenter.works</a> hat mit <a href="https://xn--berwachungspaket-izb.at/" >überwachungspaket.at</a> eine Webseite erstellt, wo sehr einfach gegen Teile des (oder das ganze) Überwachungspaket Stellung genommen werden kann. Außerdem finden sich dort Informationen über die Risiken der einzelnen Gesetzesvorschläge.
+							<br /><br />
+							Leider <a href="https://netzpolitik.org/2017/oesterreichisches-justizministerium-blockiert-protestmails-von-buergern/" hreflang="de" >blockiert das Innenministerium Protestmails von Bürgern</a>, die über diese Webseite erstellt wurden. Das kann aber ganz einfach umgangen werden, indem man die Kopie der Stellungnahme, die man per Mail erhält, an die in dem hier verlinkten <a href="https://www.parlament.gv.at/PAKT/VHG/XXV/ME/ME_00325/fname_646630.pdf" hreflang="de">Dokument</a> angeführten Mailadressen weiterleitet.
+							</p>
+							
+
+
+
+
+
+
+							
+						</div>
+					</div>					
+	
+				</div>
+	
+		</section>
+		<?php 
+			include '../../template/footer/footer.php';
+		?>	
+		<script src="/js/lightbox-plus-jquery.min.js"></script>
+	</body>
+</html>
